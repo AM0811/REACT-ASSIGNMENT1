b73b0e98dbffa0ff5a23ad3ba8651a8781ea16e7
AM0811 <21445834@student.uwl.ac.uk>
2021-11-05 11:24:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-e4hvks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-e4hvks)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+//ASSIGNMENT 1
+import React from "react";
+import "./style.css";
+import MedicalTreatment from "./MedicalTreatment";
+
+
+//Q2 one instance of the MedicalTreatment
+const treatId1  = "AS248";
+const treatCourseId1 = "1";
+const type1 = "chemo";
+const category1 = "1";
+const name1 = "Agnes";
+const startDate1 = "02/11/2021"; 
+
+export default function App() {
+  return (
+    <div>
+      <MedicalTreatment name = {name1} treatId = {treatId1} treatCourseId = {treatCourseId1} type = {type1} category = {category1} startDate = {startDate1}/>
+    </div>
+  );
+}
