430804aa2cc4a170286fe9e7bb173c5954e65689
AM0811 <21445834@student.uwl.ac.uk>
2021-11-12 11:42:16 +0000

    1 file modified, 1 added, 1 deleted

--- src/MedicalTreatmentPanel.js
+++ src/MedicalTreatmentPanel.js
@@ -1,65 +1,9 @@
-import React from "react";
+import  React from "react";
+
 
 function MedicalTreatmentPanel(){
-
-  //React Hooks
-  const[medicaltreatmenttreatId, setMedicaltreatmentTreatId] = React.useState('');
-  const[medicaltreatmenttreatCourseId, setMedicaltreatmentTreatCourseId] = React.useState('');
-  const[medicaltreatmenttype, setMedicaltreatmentType] = React.useState('');
-  const[medicaltreatmentcategory, setMedicaltreatmentCategory] = React.useState('');
-  const[medicaltreatmentname, setMedicaltreatmentName] = React.useState('');
-  const[medicaltreatmentstartdate, setMedicaltreatmentstartDate] = React.useState(Date());
-
-
-  function eventHandler(){
-    window.alert("[" + "name = " + medicaltreatmentname + ", treatId = "+ medicaltreatmenttreatId +  ", treatCourseId = "+ medicaltreatmenttreatCourseId + ", type = "+ medicaltreatmenttype + ", category = "+ medicaltreatmentcategory + ", startDate = "+ medicaltreatmentstartDate + "]");
-  }
-
-  // function onChangeHandler(){
-  //   setTreatId('');
-  //   setTreatCourseId('');
-  //   setType('');
-  //   setCategory('');
-  //   setName('');
-  //   setStartDate('');
-  // }
-
-  
-
-  return(
-    <div>
-      <input placeholder= "Treatment Id" value={medicaltreatmenttreatId} onChange ={e => setMedicaltreatmentTreatId(e.target.value)}/>
-
-      <br/>
-
-      <input placeholder= "Treatment Course Id" value={medicaltreatmenttreatCourseId} onChange ={e => setMedicaltreatmentTreatCourseId(e.target.value)}/>
-
-      <br/>
-
-      <input placeholder= "Type" value={medicaltreatmenttype} onChange ={e => setMedicaltreatmentType(e.target.value)}/>
-
-      <br/>
-      
-      <input placeholder= "Category" value={medicaltreatmentcategory} onChange ={e => setMedicaltreatmentCategory(e.target.value)}/>
-
-      <br/>
-      
-      <input placeholder= "Name" value={medicaltreatmentname} onChange ={e => setMedicaltreatmentName(e.target.value)}/>
-
-      <br/>
-      
-      <input placeholder= "Start Date" value={medicaltreatmentstartdate} onChange ={e => setMedicaltreatmentstartDate(e.target.value)}/>
-
-      <br/>
-      {/* {treatId}
-      {treatCourseId} */}
-
-      <button onClick = {eventHandler}>readanddisplay</button>
-
-        </div> 
-
-
-  )
+  const [treatId, setTreatId] = React.useState('');
+  const [treatCourseId, setTreatCourseId] = React.useState('');
+  const [type, setType] = React.useState('');
+  const [category, setCategory] = React.useState('');
 }
-
-export default MedicalTreatmentPanel;
